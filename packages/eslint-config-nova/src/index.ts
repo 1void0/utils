dfafefc4fbe2f7edcd65b4a2063f5b693b1e56ea
Cédric Trigoso <cedric@niriu.com>
2019-05-29 11:28:59 +0200

    feat: eslint-config-prettier added 2 new rules

--- packages/eslint-config-nova/src/index.ts
+++ packages/eslint-config-nova/src/index.ts
@@ -18,8 +18,8 @@ module.exports = {
     './rules/es6',
     './rules/typescript',
     './rules/import',
-    './rules/prettier',
-    './rules/jest'
+    './rules/jest',
+    './rules/prettier'
   ].map((file: string): string => require.resolve(file)),
   parserOptions: {
     ecmaVersion: 2019 // Allows for the parsing of modern ECMAScript features
