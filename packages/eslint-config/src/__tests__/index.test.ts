e0a5566a069cfc78cb36cf410c6cf9dbafb4b85a
Cédric Trigoso <cedric@niriu.com>
2019-06-10 16:08:03 +0200

    style: run prettier

--- packages/eslint-config/src/__tests__/index.test.ts
+++ packages/eslint-config/src/__tests__/index.test.ts
@@ -7,30 +7,28 @@ import path from 'path'
 const eslintConfig = require('../')
 
 const getFiles = async (): Promise<object> =>
-  new Promise(
-    (resolve, reject): void => {
-      fs.readdir(
-        path.join(__dirname, '../rules'),
-        (error, directoryFiles): void => {
-          if (error) {
-            reject(error)
-          }
-
-          const files = {}
-
-          directoryFiles.forEach(
-            async (name): Promise<void> => {
-              files[name] = await import(
-                /* webpackChunkName: "rule" */ `../rules/${name}`
-              )
-            }
-          )
-
-          resolve(files)
+  new Promise((resolve, reject): void => {
+    fs.readdir(
+      path.join(__dirname, '../rules'),
+      (error, directoryFiles): void => {
+        if (error) {
+          reject(error)
         }
-      )
-    }
-  )
+
+        const files = {}
+
+        directoryFiles.forEach(
+          async (name): Promise<void> => {
+            files[name] = await import(
+              /* webpackChunkName: "rule" */ `../rules/${name}`
+            )
+          }
+        )
+
+        resolve(files)
+      }
+    )
+  })
 
 describe('@void0/eslint-config', (): void => {
   test('extends all files', async (): Promise<void> => {
@@ -51,10 +49,8 @@ describe('@void0/eslint-config', (): void => {
     expect.hasAssertions()
     const files = await getFiles()
 
-    Object.keys(files).forEach(
-      (name): void => {
-        expect(files[name].rules).toBeInstanceOf(Object)
-      }
-    )
+    Object.keys(files).forEach((name): void => {
+      expect(files[name].rules).toBeInstanceOf(Object)
+    })
   })
 })
