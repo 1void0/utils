c81c5ae30f8c23d3cfdf653ec57273dc9cd7e8f8
Cédric Trigoso <cedric@niriu.com>
2020-06-23 15:57:06 +0200

    feat: bump version

--- packages/commitlint-config/jest.config.js
+++ packages/commitlint-config/jest.config.js
@@ -1,11 +1,8 @@
 /* eslint-env commonjs */
 
-/* eslint-disable import/no-commonjs */
-/* eslint-disable import/unambiguous */
-
 module.exports = {
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 }
